59b5e338b8e5d049184c350e0933bad975da22cc
lioudpl04 <lp2000160@gmail.com>
2025-12-28 21:51:25 +0200

    Initial Spring Boot project

---
+++ src/main/java/gr/uni/cinema/cinemamanagment/CinemaManagmentApplication.java
@@ -0,0 +1,13 @@
+package gr.uni.cinema.cinemamanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/uni/cinema/cinemamanagment/CinemaManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uni.cinema.cinemamanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
